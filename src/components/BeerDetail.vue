87a56474ce752a2214e5d3f43b2f3e45cd175de9
roosap <roosa.paivansalo@gmail.com>
2020-02-27 11:06:43 +0000

    Initial commit

---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div v-if="beer">
+    <h3>{{beer.name}}</h3>
+    <p>Description: {{beer.description}}</p>
+    <p>Alcohol by volume: {{beer.abv}}</p>
+    <p>First brewed: {{beer.first_brewed}}</p>
+    <img :src="beer.image_url">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'beer-detail',
+  props: ['beer']
+}
+</script>
+
+<style lang="css" scoped>
+  img {
+    height: 15em;
+    width: auto;
+  }
+</style>
---
+++ src/components/BeersList.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <ul>
+      <list-item v-for="(beer, index) in beers" :beer="beer" :key="index"></list-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+
+export default {
+  name: 'beers-list',
+  props: ['beers'],
+  components: {
+    "list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
